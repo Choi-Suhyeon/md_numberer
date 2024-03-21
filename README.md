d515958567a3b57caf19d3c8c9357005a3f03990
SUHYEON CHOI <1AzyF0x@protonmail.ch>
2024-03-21 12:31:05 +0900

    Add options for starting header numbering and starting number for numbering, along with related functionality.

--- README.md
+++ README.md
@@ -4,9 +4,19 @@ Herein lies a program that provides numbering for each heading within a markdown
 마크다운 파일 내의 각 제목에 대해 넘버링을 해주는 프로그램입니다.
 
 ## USEAGE GUIDELINES
-`md_numberer --file | --directory <FILE_OR_DIRECTORY_PATH>`
+```
+sample : md_numberer --file sample.md -l 2 -s 1
 
+syntax :
+    md_numberer --help | -h
+    md_numberer --file <file_name> [-l <header_number_limit>] [-s <starting_number>]
+    md_numberer --directory <directory_name> [-l <header_number_limit>] [-s <starting_number>]
+```
+
+- `-l` : 넘버링을 시작할 헤드 번호. 입력 범위 : [1, 6]. 기본값 : 1
+- `-s` : 넘버링을 시작할 번호. 입력 범위 : [0, 1]. 기본값 : 1
+ 
 ## TO-DO LIST
-1. `h1`부터 `h6` 중 어디부터 어디까지 넘버링을 할지 옵션 넣기.
-1. 넘버링을 1부터 시작할지 0부터 시작할지 옵션 넣기.
+1. ~~`h1`부터 `h6` 중 어디부터 넘버링을 할지 옵션 넣기.~~
+1. ~~넘버링을 1부터 시작할지 0부터 시작할지 옵션 넣기.~~
 1. ~~폴더를 지정하면 폴더 내 모든 마크다운 파일에 대해 넘버링을 적용시키는 옵션 넣기.~~
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,82 @@
-use std::io::{Read, Write};
+use std::fmt::{Display, Formatter};
+use std::io::{self, Read, Write};
 use std::fs::{self, File};
+use std::process::exit;
 use std::error::Error;
 use std::env;
 
+type GenericError = Box<dyn Error + Send + Sync + 'static>;
+type GenericResult<T> = Result<T, GenericError>;
+
+#[derive(Debug, Clone)]
+struct EmptyFileError;
+
+impl Error for EmptyFileError {}
+
+impl Display for EmptyFileError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "the target file is empty")
+    }
+}
+
+#[derive(Debug, Clone)]
+struct MarkdownNotFoundError;
+
+impl Error for MarkdownNotFoundError {}
+
+impl Display for MarkdownNotFoundError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "the target file does not have a markdown extension")
+    }
+}
+
 trait MdNumberer {
+    fn add_prefix_number(&mut self, limit: usize, starting_num: usize);
     fn delete_prefix_number(&mut self);
-    fn add_prefix_number(&mut self);
 }
 
 impl MdNumberer for Vec<String> {
+    fn add_prefix_number(&mut self, limit: usize, starting_num: usize) {
+        let starting_num = starting_num as isize - 1;
+
+        let mut numbers = [starting_num; 7];
+
+        for i in self.iter_mut() {
+            if i.is_empty() {
+                continue;
+            }
+
+            let mut split = (*i)
+                .splitn(2, ' ')
+                .map(String::from)
+                .collect::<Vec<_>>();
+
+            let num_of_hashtags = split[0].chars().count();
+
+            if num_of_hashtags < limit || !split[0].chars().all(|x| x == '#') {
+                continue;
+            }
+
+            numbers[num_of_hashtags] += 1;
+
+            for j in num_of_hashtags + 1 .. numbers.len() {
+                numbers[j] = starting_num;
+            }
+
+            split.insert(
+                1,
+                numbers[limit ..= num_of_hashtags]
+                    .iter()
+                    .map(|x| x.to_string())
+                    .collect::<Vec<_>>()
+                    .join(".")
+                    + ".",
+            );
+
+            *i = split.join(" ");
+        }
+    }
+
     fn delete_prefix_number(&mut self) {
         for i in self.iter_mut() {
             if i.is_empty() {
@@ -29,80 +97,69 @@ impl MdNumberer for Vec<String> {
             *i = split.join(" ");
         }
     }
-
-    fn add_prefix_number(&mut self) {
-        let mut numbers = [0usize; 7];
-
-        for i in self.iter_mut() {
-            if i.is_empty() {
-                continue;
-            }
-
-            let mut split = (*i)
-                .splitn(2, ' ')
-                .map(String::from)
-                .collect::<Vec<_>>();
-
-            let fst_word        = split[0]
-                .chars()
-                .collect::<Vec<_>>();
-            let num_of_hashtags = fst_word.len();
-
-            if num_of_hashtags < 2 || !fst_word.iter().all(|&x| x == '#') {
-                continue;
-            }
-
-            numbers[num_of_hashtags] += 1;
-
-            for j in num_of_hashtags + 1 .. numbers.len() {
-                numbers[j] = 0;
-            }
-
-            split.insert(
-                1, 
-                numbers[2 ..= num_of_hashtags]
-                    .iter()
-                    .map(usize::to_string)
-                    .collect::<Vec<_>>()
-                    .join(".")
-                    + ".",
-            );
-
-            *i = split.join(" ");
-        }
-    }
 }
 
-fn apply_toc_to_dir(target_dir: String) -> Result<(), Box<dyn Error>> {
+fn apply_number_to_dir(target_dir: String, limit: usize, starting_num: usize) -> GenericResult<()> {
     let paths = fs::read_dir(target_dir)?;
 
     for path in paths {
-        let _ = apply_toc(path?.path().to_str().unwrap().to_owned());
+        let Ok(path) = path else {
+            continue;
+        };
+
+        let path = path
+            .path()
+            .to_str()
+            .unwrap()
+            .to_owned();
+
+        match apply_number(path.clone(), limit, starting_num) {
+            Ok(()) => continue,
+            Err(err) => {
+                if let Some(_) = err.downcast_ref::<MarkdownNotFoundError>() {
+                    continue;
+                }
+                else {
+                    let msg_string =
+                        if let Some(msg) = err.downcast_ref::<EmptyFileError>() {
+                            msg.to_string()
+                        }
+                        else if let Some(msg) = err.downcast_ref::<io::Error>() {
+                            msg.to_string()
+                        }
+                        else {
+                            err.to_string()
+                        };
+
+                    println!("[FAIL] {} : {}", path, msg_string);
+                }
+            },
+        }
     }
 
     Ok(())
 }
 
-fn apply_toc(target_path: String) -> Result<(), Box<dyn Error>> {
+fn apply_number(target_path: String, limit: usize, starting_num: usize) -> GenericResult<()> {
     let mark_comment = String::from("<!--worked by md_numberer. DO NOT EDIT this line-->");
 
     if &target_path[target_path.len() - 3 ..] != ".md" {
-        return Err(Box::from("is not a markdown file"));
+        return Err(GenericError::from(MarkdownNotFoundError));
     }
 
     let mut file     = File::open(target_path.clone())?;
     let mut contents = String::new();
 
-    file
-        .read_to_string(&mut contents)?;
+    file.read_to_string(&mut contents)?;
 
     let mut contents = contents
+        .trim()
         .split('\n')
         .map(String::from)
-        .collect::<Vec<String>>();
+        .collect::<Vec<_>>();
 
-    if contents.is_empty() {
-        return Err(Box::from("file has no content"));
+    if contents.len() == 1 && contents[0] == "" {
+        return Err(GenericError::from(EmptyFileError));
     }
 
     if contents.last().unwrap() == &mark_comment {
@@ -112,7 +169,7 @@ fn apply_toc(target_path: String) -> Result<(), Box<dyn Error>> {
         contents.push(mark_comment);
     }
 
-    contents.add_prefix_number();
+    contents.add_prefix_number(limit, starting_num);
 
     let mut file = File::create(target_path)?;
 
@@ -124,21 +181,77 @@ fn apply_toc(target_path: String) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn main() {
-    let mut args = env::args().skip(1);
+fn print_help(file_name: String) {
+    println!("sample : {} --file sample.md -l 2 -s 1", file_name);
+    println!("syntax :");
+    println!("    {} --help | -h", file_name);
+    println!("    {} --file <file_name> [-l <header_number_limit>] [-s <starting_number>]", file_name);
+    println!("    {} --directory <directory_name> [-l <header_number_limit>] [-s <starting_number>]", file_name);
+    println!("range :");
+    println!("    header_number_limit(integer; default: 1) : [1, 6]");
+    println!("    starting_number(integer; default: 1)     : [0, 1]");
+}
 
+fn main() {
+    let mut args = env::args();
+    let mut limit = 1;
+    let mut starting_num = 1;
+
+    let file_name = args.next().unwrap();
     let is_dir = match args.next() {
         Some(x) if &x == "--directory" => true,
         Some(x) if &x == "--file" => false,
-        _ => panic!("invalid format"),
+        Some(x) if &x == "-h" || &x == "--help" => {
+            print_help(file_name);
+            exit(0);
+        },
+        _ => panic!("required option does not exist"),
     };
-    let target_path = args.next();
+    let target_path = args
+        .next()
+        .expect("option is given without a parameter");
 
-    if target_path.is_none() || args.count() != 0 {
-        panic!("invalid format");
+    loop {
+        match args.next() {
+            Some(x) => {
+                *match &*x {
+                    "-l" => &mut limit,
+                    "-s" => &mut starting_num,
+                    _ => panic!("illegal option is given"),
+                } = args
+                    .next()
+                    .expect("option is given without a parameter")
+                    .parse()
+                    .expect("parameter format is illegal")
+            },
+            None => {
+                if (0..=1).contains(&starting_num) || (1..=6).contains(&limit) {
+                    break;
+                }
+
+                panic!("parameter range is illegal");
+            },
+        }
     }
 
-    let target_path  = target_path.unwrap();
+    match (if is_dir { apply_number_to_dir } else { apply_number })(target_path.clone(), limit, starting_num) {
+        Ok(()) => (),
+        Err(err) => {
+            let msg_string =
+                if let Some(msg) = err.downcast_ref::<MarkdownNotFoundError>() {
+                    msg.to_string()
+                }
+                else if let Some(msg) = err.downcast_ref::<EmptyFileError>() {
+                    msg.to_string()
+                }
+                else if let Some(msg) = err.downcast_ref::<io::Error>() {
+                    msg.to_string()
+                }
+                else {
+                    err.to_string()
+                };
 
-    (if is_dir { apply_toc_to_dir } else { apply_toc })(target_path).unwrap();
+            println!("[FAIL] {} : {}", target_path, msg_string);
+        },
+    }
 }
